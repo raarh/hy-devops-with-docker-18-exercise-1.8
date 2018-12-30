4eccf276027517218530ca13c2d62ef7c7374a9f
Arto Rantanen <arto.rantanen@helsinki.fi>
2018-12-30 14:43:40 +0200

    Add dummy aspnetcore mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sturdy_journey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+[DevOps with docker](https://docker-hy.github.io/exercises/) exercise 1.8
